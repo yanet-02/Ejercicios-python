bf8e7e553c7d4e64a54f3044e10a0f38aa0a3be5
yanet-02 <jenamoradagarcia@gmail.com>
2025-02-11 15:23:40 -0600

    Sexto Ejercicio

---
+++ ejercicio6.py
@@ -0,0 +1,9 @@
+numero1=float(input("Ingrese el primer numero: "))
+numero2=float(input("Ingrese el segundo numero: "))
+nombre=(input("Tu nombre: "))
+
+resultado_res = numero1 - numero2
+resultado_multi = numero1 * numero2
+print(f"El resultado de la resta de {numero1} - {numero2} es: {resultado_res}\n")
+print(f"El resultado de la multiplicación de {numero1} * {numero2} es: {resultado_multi}\n")
+print(f"Programa realizado por: {nombre}")
