ca6819ce32c576d1c352f38aae1aaaa3cc95e57c
yanet-02 <jenamoradagarcia@gmail.com>
2025-02-11 15:16:26 -0600

    Cuarto Ejercicio

---
+++ Ejercicio 4.py
@@ -0,0 +1,6 @@
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+altura = float(input("¿Cuánto mides? (en metros) "))
+
+print("Hola", nombre, "tienes", edad, "años y mides", altura, "metros.")
+
