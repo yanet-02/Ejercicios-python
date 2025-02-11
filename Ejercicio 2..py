b524b1e633e9e2bfc1945a8859b482d74cb3dfbe
yanet-02 <jenamoradagarcia@gmail.com>
2025-02-11 14:59:58 -0600

    Segundo Ejercicio

---
+++ Ejercicio 2..py
@@ -0,0 +1,13 @@
+# Declarar variables
+nombre = "Yanet"
+edad = 25
+altura = 1.75
+
+# Imprimir información
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Altura:", altura)
+
+# Operación simple
+edad_en_5_anos = edad + 5
+print("En 5 años, tendré", edad_en_5_anos, "años.")
